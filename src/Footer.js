c70bc99b86be5603d6f15650f323220305a9a61b
marnicodes <marni.ali.codes@gmail.com>
2021-11-16 11:15:10 +0100

    Changed Footer details

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="Footer">
+      <a href="https://github.com/marnicodes/weather-react-app">
+        Open-source code,
+      </a>{" "}
+      by <a href="https://peaceful-wiles-ea9b0d.netlify.app/">Marni Ali</a>
+    </div>
+  );
+}
