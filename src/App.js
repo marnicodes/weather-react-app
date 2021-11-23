97bedba624ef038a2e05e24503647ce4c74e067e
marnicodes <marni.ali.codes@gmail.com>
2021-11-23 21:39:48 +0100

    Reorganised Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Weather from "./Weather";
 import Footer from "./Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App(props) {
   return (
@@ -8,15 +9,6 @@ function App(props) {
       <div className="box">
         {" "}
         <Weather />
-        <h1>(City)</h1>
-        <div className="last-updated">Last updated:</div>
-        <div className="container">
-          <div className="row">
-            <div className="col-4">(Icon) </div>
-            <div className="col-4">(Temperature)</div>
-            <div className="col-4">(WeatherDetails)</div>
-          </div>
-        </div>
       </div>
       <Footer />
     </div>
